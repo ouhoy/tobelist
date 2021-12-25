e89fa2a67884532a690d45041ec80f2f961145c3
ouhoy <kimkinon@gmail.com>
2021-12-25 19:16:12 +0100

    add few fixes to the html and css files
    Just moving some lines here and there nothing has really changed

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+function $(id) {
+    return document.querySelectorAll(id)
+}
+const userReview = $(".user-review");
+const reviews = document.querySelector(".reviews");
+const nextBtn = document.querySelector(".arrow-right");
+const prevBtn = document.querySelector(".arrow-left");
+const line = document.querySelector(".line");
+function init() {
+    
+}
