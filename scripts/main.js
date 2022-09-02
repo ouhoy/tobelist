cc3f7860326552780988d4aa01d16fbc3cf72c34
ouhoy <kimkinon@gmail.com>
2022-09-02 18:29:28 +0100

    Update the UI of the header

--- scripts/main.js
+++ scripts/main.js
@@ -1,119 +1,93 @@
-function $(id) {
-    return document.querySelector(id)
+function $(id, slectAll = false) {
+  return slectAll ? document.querySelectorAll(id) : document.querySelector(id);
 }
+
 const reviewsConainer = document.querySelectorAll(".reviews-container");
 const reviewsSection = $(".reviews-section");
 const arrowRight = $(".arrow-right");
 const arrowLeft = $(".arrow-left");
 const lines = $(".lines");
 const hamMenu = $(".ham-menu");
-const hamburgerMenu= $(".hamburger-menu");
+const hamburgerMenu = $(".hamburger-menu");
 
-
-hamburgerMenu.addEventListener("click", ()=>{
-    hamMenu.classList.toggle("hide");
-    document.body.classList.toggle("overflow-hiden")
-
-})
+hamburgerMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("hide");
+  document.body.classList.toggle("overflow-hiden");
+});
 
 const plus = document.getElementsByClassName("plus");
-const faqs =  document.getElementsByClassName("faqs");
+const faqs = document.getElementsByClassName("faqs");
 const faqsText = document.querySelectorAll(".faqs-text");
 const plusLine = document.querySelectorAll(".plus-line");
- 
-for (let i = 0; i < faqs.length; i++) {
-    plus[i].addEventListener("click", function(){
-        this.classList.toggle(
-            faqsText[i].classList.toggle("hide")
-        );
-        this.classList.toggle(plusLine[i].classList.toggle("plus-rotate"));
-    })
-    
-}
 
+for (let i = 0; i < faqs.length; i++) {
+  plus[i].addEventListener("click", function () {
+    this.classList.toggle(faqsText[i].classList.toggle("hide"));
+    this.classList.toggle(plusLine[i].classList.toggle("plus-rotate"));
+  });
+}
 
 var currentSlide = 0;
 
 var numberOfReviews = reviewsConainer.length;
 
-
 function init() {
-    reviewsConainer.forEach((review, i)=>{
-        review.style.left = i * 100 + "%"
-    })
-};
+  reviewsConainer.forEach((review, i) => {
+    review.style.left = i * 100 + "%";
+  });
+}
 init();
 
-for (var i =0; i < numberOfReviews; i++ ) {
-    linesMaker()
-    var lineBackground = document.querySelectorAll(".line");
-
+for (var i = 0; i < numberOfReviews; i++) {
+  linesMaker();
+  var lineBackground = document.querySelectorAll(".line");
 }
 
 function linesMaker(num) {
-    const line =  document.createElement("div");
-    line.classList.add("line")
-    lines.appendChild(line)
-   
+  const line = document.createElement("div");
+  line.classList.add("line");
+  lines.appendChild(line);
 }
 
 function changeLineColor(before, after) {
-    lineBackground[currentSlide].classList.add("silected-slide");
-
-
+  lineBackground[currentSlide].classList.add("silected-slide");
 }
-var lineColor =  changeLineColor();
+var lineColor = changeLineColor();
 
+arrowRight.addEventListener("click", () => {
+  currentSlide++;
+  lineBackground[currentSlide - 1].classList.remove("silected-slide");
 
-
-arrowRight.addEventListener("click", ()=>{
-    
-    currentSlide++;
-    lineBackground[currentSlide -1].classList.remove("silected-slide");
-
-    if (currentSlide >= numberOfReviews ) {
-        goToSLide(0);
-        return  
-    }
-    goToSLide(currentSlide);
-
+  if (currentSlide >= numberOfReviews) {
+    goToSLide(0);
+    return;
+  }
+  goToSLide(currentSlide);
 });
-arrowLeft.addEventListener("click", ()=>{
+arrowLeft.addEventListener("click", () => {
+  if (currentSlide <= 0) {
+    lineBackground[currentSlide + 1].classList.remove("silected-slide");
+    lineBackground[0].classList.remove("silected-slide");
 
-    if (currentSlide  <= 0) {
-        lineBackground[currentSlide +1].classList.remove("silected-slide");
-        lineBackground[0].classList.remove("silected-slide");
+    goToSLide(numberOfReviews - 1);
 
-        goToSLide(numberOfReviews - 1);
-
-        return
-    }
-    currentSlide--;
-    lineBackground[currentSlide +1].classList.remove("silected-slide");
-    goToSLide(currentSlide)
+    return;
+  }
+  currentSlide--;
+  lineBackground[currentSlide + 1].classList.remove("silected-slide");
+  goToSLide(currentSlide);
 });
 
 function goToSLide(slideNumber) {
+  reviewsSection.style.transform = "translateX(-" + 100 * slideNumber + "%)";
+  const crurrentSlider = currentSlide;
+  currentSlide = slideNumber;
 
-    reviewsSection.style.transform =
-    "translateX(-" + 100 * slideNumber + "%)";
-    const crurrentSlider = currentSlide;
-    currentSlide = slideNumber;
- 
-    changeLineColor(crurrentSlider, slideNumber)
-
-
+  changeLineColor(crurrentSlider, slideNumber);
 }
 
-for (let i = 0; i <numberOfReviews; i++) {
-
-
-    lineBackground[i].addEventListener("click", ()=>{
-        goToSLide(i)
-    })
-    
-    
+for (let i = 0; i < numberOfReviews; i++) {
+  lineBackground[i].addEventListener("click", () => {
+    goToSLide(i);
+  });
 }
-
-   
-
