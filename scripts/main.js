868d765cf0f876520566db660817d57d66e59cdd
ouhoy <kimkinon@gmail.com>
2022-09-09 05:12:01 +0100

    Make the FAQs question container work

--- scripts/main.js
+++ scripts/main.js
@@ -9,85 +9,22 @@ const arrowLeft = $(".arrow-left");
 const lines = $(".lines");
 const hamMenu = $(".ham-menu");
 const hamburgerMenu = $(".hamburger-menu");
+const plus = $(".plus");
+const faqs = $(".faqs");
+const faqsText = $(".faqs-text", true);
+const plusLine = $(".plus-line", true);
 
 hamburgerMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("hide");
   document.body.classList.toggle("overflow-hiden");
 });
 
-const plus = document.getElementsByClassName("plus");
-const faqs = document.getElementsByClassName("faqs");
-const faqsText = document.querySelectorAll(".faqs-text");
-const plusLine = document.querySelectorAll(".plus-line");
-
-for (let i = 0; i < faqs.length; i++) {
-  plus[i].addEventListener("click", function () {
-    this.classList.toggle(faqsText[i].classList.toggle("hide"));
-    this.classList.toggle(plusLine[i].classList.toggle("plus-rotate"));
-  });
-}
-
-var currentSlide = 0;
-
-var numberOfReviews = reviewsConainer.length;
-
-function init() {
-  reviewsConainer.forEach((review, i) => {
-    review.style.left = i * 100 + "%";
-  });
-}
-init();
-
-for (var i = 0; i < numberOfReviews; i++) {
-  linesMaker();
-  var lineBackground = document.querySelectorAll(".line");
-}
-
-function linesMaker(num) {
-  const line = document.createElement("div");
-  line.classList.add("line");
-  lines.appendChild(line);
-}
-
-function changeLineColor(before, after) {
-  lineBackground[currentSlide].classList.add("silected-slide");
-}
-var lineColor = changeLineColor();
-
-arrowRight.addEventListener("click", () => {
-  currentSlide++;
-  lineBackground[currentSlide - 1].classList.remove("silected-slide");
-
-  if (currentSlide >= numberOfReviews) {
-    goToSLide(0);
-    return;
+faqs.addEventListener("click", function (e) {
+  if (e.target.closest(".question-container")) {
+    console.log("ok");
+    e.target.closest(".question-container").classList.toggle("show-question");
+    e.target
+      .closest(".question-container")
+      .children[0].children[1].children[1].classList.toggle("rotate-line");
   }
-  goToSLide(currentSlide);
 });
-arrowLeft.addEventListener("click", () => {
-  if (currentSlide <= 0) {
-    lineBackground[currentSlide + 1].classList.remove("silected-slide");
-    lineBackground[0].classList.remove("silected-slide");
-
-    goToSLide(numberOfReviews - 1);
-
-    return;
-  }
-  currentSlide--;
-  lineBackground[currentSlide + 1].classList.remove("silected-slide");
-  goToSLide(currentSlide);
-});
-
-function goToSLide(slideNumber) {
-  reviewsSection.style.transform = "translateX(-" + 100 * slideNumber + "%)";
-  const crurrentSlider = currentSlide;
-  currentSlide = slideNumber;
-
-  changeLineColor(crurrentSlider, slideNumber);
-}
-
-for (let i = 0; i < numberOfReviews; i++) {
-  lineBackground[i].addEventListener("click", () => {
-    goToSLide(i);
-  });
-}
