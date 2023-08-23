78a4dbef626e3bf6d91cbaf9c702678b4bc3e6ff
ouhoy <kimkinon@gmail.com>
2023-08-23 22:18:19 +0100

    Add explore section

---
+++ src/components/sections/Explore.tsx
@@ -0,0 +1,17 @@
+export default function Explore() {
+    return <section className="explore">
+        <div className="explore-section">
+            <h2>Make your life more efficient</h2>
+            <p>
+                Tobelist is the ultimate form of boosting productivity. Unlike many
+                other <br/>
+                tools out there, this one can incorporate multiple methods.
+            </p>
+            <img
+                src="./images/tobelist app.svg"
+                className="section section--hidden"
+                alt="tobelist web app"
+            />
+        </div>
+    </section>
+}
